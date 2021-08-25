f5adcab4caa8074bd200de9084952876426fad3f
IaraLima <iarasantos.cabluelo@gmail.com>
2021-08-25 16:41:20 -0300

    corrigido erro vol_grid

--- 3BS/triBS_ex.py
+++ 3BS/triBS_ex.py
@@ -1,23 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug  6 12:29:30 2021
-
-@author: iara
-"""
-
-
-
 import meep as mp
 import numpy
-import gc
 #from meep.materials import SiO2
 #from meep.materials import Si3N4
 import matplotlib.pyplot as plt
 
-res = 50        # pixels/μm
+#BS com o material que iremos usar nos chips e o comprimento de onda tbm 0.8
+#
+res = 25        # pixels/μm
 three_d = False # 3d calculation?
-d = 0.02        # branch separation
+d = 0.1125        # branch separation
 
 gdsII_file = 'triBS.gds'
 CELL_LAYER = 0
@@ -29,9 +20,10 @@ PORT5_LAYER = 5
 PORT6_LAYER = 6
 SOURCE_LAYER = 7
 UPPER_BRANCH_LAYER = 31
-MEIO_BRANCH_LAYER = 33
 LOWER_BRANCH_LAYER = 32
+MEIO_BRANCH_LAYER = 33
 
+default_d = 0.2
 
 t_oxide = 1.0
 t_Si = 0.22
@@ -40,11 +32,10 @@ t_air = 0.78
 dpml = 1
 cell_thickness = dpml+t_oxide+t_Si+t_air+dpml
 
+oxide = mp.Medium(epsilon=2.0736)
+silicon= mp.Medium(epsilon=4.0804)
 
-oxide = mp.Medium(epsilon=2.25)
-silicon= mp.Medium(epsilon=12)
-
-lcen = 0.8
+lcen = 0.795
 fcen = 1/lcen
 df = 0.2*fcen
 
@@ -59,8 +50,6 @@ upper_branch = mp.get_GDSII_prisms(silicon, gdsII_file, UPPER_BRANCH_LAYER, si_z
 lower_branch = mp.get_GDSII_prisms(silicon, gdsII_file, LOWER_BRANCH_LAYER, si_zmin, si_zmax)
 meio_branch = mp.get_GDSII_prisms(silicon, gdsII_file, MEIO_BRANCH_LAYER, si_zmin, si_zmax)
 
-
-
 cell = mp.GDSII_vol(gdsII_file, CELL_LAYER, cell_zmin, cell_zmax)
 p1 = mp.GDSII_vol(gdsII_file, PORT1_LAYER, si_zmin, si_zmax)
 p2 = mp.GDSII_vol(gdsII_file, PORT2_LAYER, si_zmin, si_zmax)
@@ -68,19 +57,17 @@ p3 = mp.GDSII_vol(gdsII_file, PORT3_LAYER, si_zmin, si_zmax)
 p4 = mp.GDSII_vol(gdsII_file, PORT4_LAYER, si_zmin, si_zmax)
 p5 = mp.GDSII_vol(gdsII_file, PORT5_LAYER, si_zmin, si_zmax)
 p6 = mp.GDSII_vol(gdsII_file, PORT6_LAYER, si_zmin, si_zmax)
-
 src_vol = mp.GDSII_vol(gdsII_file, SOURCE_LAYER, si_zmin, si_zmax)
 
 # displace upper and lower branches of coupler (as well as source and flux regions)
-"""if d != default_d:
+'''
+if d != default_d:
     delta_y = 0.5*(d-default_d)
     delta = mp.Vector3(y=delta_y)
     p1.center += delta
     p2.center -= delta
     p3.center += delta
     p4.center -= delta
-  #  p5.center += delta
-  #  p6.center -=delta
     src_vol.center += delta
     cell.size += 2*delta
     for np in range(len(lower_branch)):
@@ -91,21 +78,18 @@ src_vol = mp.GDSII_vol(gdsII_file, SOURCE_LAYER, si_zmin, si_zmax)
         upper_branch[np].center += delta
         for nv in range(len(upper_branch[np].vertices)):
             upper_branch[np].vertices[nv] += delta
-    for np in range(len(meio_branch)):
-        meio_branch[np].center += delta
-        for nv in range(len(meio_branch[np].vertices)):
-            meio_branch[np].vertices[nv] += delta
- """           
+'''
 geometry = upper_branch+meio_branch+lower_branch
+
 if three_d:
     oxide_center = mp.Vector3(z=-0.5*t_oxide)
     oxide_size = mp.Vector3(cell.size.x,cell.size.y,t_oxide)
     oxide_layer = [mp.Block(material=oxide, center=oxide_center, size=oxide_size)]
     geometry = geometry+oxide_layer
-'''
+
 sources = [mp.EigenModeSource(src=mp.GaussianSource(fcen,fwidth=df),
-                              size=src_vol.size,
-                              center=src_vol.center,
+                              volume=src_vol,
+                              direction = mp.X,
                               eig_band=1,
                               eig_parity=mp.NO_PARITY if three_d else mp.EVEN_Y+mp.ODD_Z,
                               eig_match_freq=True)]
@@ -116,7 +100,6 @@ sim = mp.Simulation(resolution=res,
                     sources=sources,
                     geometry=geometry)
 
-# entender esses comandos!!
 mode1 = sim.add_mode_monitor(fcen, 0, 1, mp.ModeRegion(volume=p1))
 mode2 = sim.add_mode_monitor(fcen, 0, 1, mp.ModeRegion(volume=p2))
 mode3 = sim.add_mode_monitor(fcen, 0, 1, mp.ModeRegion(volume=p3))
@@ -134,8 +117,6 @@ p4_coeff = sim.get_eigenmode_coefficients(mode4, [1], eig_parity=mp.NO_PARITY if
 p5_coeff = sim.get_eigenmode_coefficients(mode5, [1], eig_parity=mp.NO_PARITY if three_d else mp.EVEN_Y+mp.ODD_Z).alpha[0,0,0]
 p6_coeff = sim.get_eigenmode_coefficients(mode6, [1], eig_parity=mp.NO_PARITY if three_d else mp.EVEN_Y+mp.ODD_Z).alpha[0,0,0]
 
-
-
 # transmittance
 p2_trans = abs(p2_coeff)**2/abs(p1_coeff)**2
 p3_trans = abs(p3_coeff)**2/abs(p1_coeff)**2
@@ -143,15 +124,13 @@ p4_trans = abs(p4_coeff)**2/abs(p1_coeff)**2
 p5_trans = abs(p5_coeff)**2/abs(p1_coeff)**2
 p6_trans = abs(p6_coeff)**2/abs(p1_coeff)**2
 
-print(p1)
+#print("trans: {:.2f}, {:.6f}, {:.6f}, {:.6f}".format(d,p2_trans,p3_trans,p4_trans))
 
-print("trans: {:.2f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}, {:.6f}".format(d,p2_trans,p3_trans,p4_trans,p5_trans,p6_trans))
-'''
-#sim.reset_meep()
+sim.reset_meep()
 
 sources = [mp.EigenModeSource(src=mp.ContinuousSource(fcen,fwidth=df),
-                              size=src_vol.size,
-                              center=src_vol.center,
+                              volume=src_vol,
+                              direction= mp.X,
                               eig_band=1,
                               eig_parity=mp.EVEN_Y+mp.ODD_Z,
                               eig_match_freq=True)]
@@ -172,4 +151,5 @@ plt.imshow(numpy.transpose(eps_data), interpolation='spline36', cmap='binary')
 plt.imshow(numpy.flipud(numpy.transpose(ez_data)), interpolation='spline36', cmap='RdBu', alpha=0.9)
 plt.axis('off')
 plt.show()
-gc.collect()
+print("trans: {:.2f}, {:.6f}, {:.6f}, {:.6f}".format(d,p2_trans,p3_trans,p4_trans))
+sim.reset_meep()
